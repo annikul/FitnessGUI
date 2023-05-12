ef5b9cd739f3bbbb0680c1156ba4e2b21c9dc670
AnniK <122267866+annikul@users.noreply.github.com>
2023-05-12 15:21:37 +0300

    Muutettu timetools paremmaksi

--- timetools.py
+++ timetools.py
@@ -32,9 +32,33 @@ def timediff(t1, t2):
     t1 = datetime.datetime.strptime(t1, "%H:%M:%S")
     t2 = datetime.datetime.strptime(t2, "%H:%M:%S")
 
-    # Function calcultates a timedelta which support only seconds or microseconds
-    seconds = abs((t2 - t1).seconds)
+    # To get absolute value chack if t2 is greater than t1
+    if t2 > t1:
+        # Function calcultates a timedelta which support only seconds or microseconds
+        seconds = abs((t2 - t1).seconds)
+    else:
+        seconds = abs((t1-t2).seconds)
+
     hours = seconds / 3600 # minute 60 seconds, hour 60 minutes
+    return hours
+
+def dateTimeDiff(start, end):
+    """Returns difference between two moments
+
+    Args:
+        start (str): date time value in format YYYY-MM-dd hh:mm:ss 
+        end (str): date time value in format YYYY-MM-dd hh:mm:ss
+
+    Returns:
+        float: difference in hours
+    """
+    v1 = datetime.datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
+    v2 = datetime.datetime.strptime(end, "%Y-%m-%d %H:%M:%S")
+    difference = v2 - v1
+
+    # Total_seconds calculates also seconds in the date part of dates
+    seconds = difference.total_seconds()
+    hours = seconds / 3600
     return hours
 
 def datediff2(d1, d2, unit):
@@ -51,9 +75,9 @@ def datediff2(d1, d2, unit):
     d1 = datetime.datetime.strptime(d1, "%Y-%m-%d")
     d2 = datetime.datetime.strptime(d2, "%Y-%m-%d")   
     difference = abs((d2 - d1).days) # Timedelta in days
-    units = {'day':1, 'year': 365}  # Dictionary for unit dividers # units = muuttuja, meidän sanakirja. Jos vastaus on day jaetaan 1 jos vuosi jaetaan 365    
+    units = {'day':1, 'year': 365, 'mont': 30}  # Dictionary for unit dividers # units = muuttuja, meidän sanakirja. Jos vastaus on day jaetaan 1 jos vuosi jaetaan 365    
     divider = units[unit]           # Choose by unit argument # units sanakirja josta haetaan unit.
-    value = difference / divider    # arvo mikä palautetaan
+    value = round(difference / divider, 1)    # arvo mikä palautetaan
     return value
 
 def timediff2(t1, t2, unit):
@@ -63,7 +87,6 @@ def timediff2(t1, t2, unit):
         t1 (str): time value in format HH:MM:SS
         t2 (str): time value in format HH:MM:SS
         unit (str: unit to return 
-
     Returns:
         float: time difference in chosen units  (Tämä on mitä docstring palauttaa)
     """
@@ -74,6 +97,39 @@ def timediff2(t1, t2, unit):
     divider = units[unit] # Choose divider according to unit argument
     value = seconds / divider
     return value
+
+def dateTimeDiff2(start, end, unit):
+    """Calculates difference between date time values in given units
+
+    Args:
+        start (str): date time value in format YYYY-MM-dd hh:mm:ss
+        end (str): date time value in format YYYY-MM-dd hh:mm:ss
+        unti (str): name of time unit: day, hour, minute or second
+    
+    Returns:
+        float: difference in given units
+    """
+    v1 = datetime.datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
+    v2 = datetime.datetime.strptime(end, "%Y-%m-%d %H:%M:%S")
+    difference = v2 - v1
+    units = {'day':86400, 'hour': 3600, 'minute': 60, 'second': 1}
+    divider = units[unit]
+    # Total_seconds calculates also seconds in the date part of dates
+    seconds= difference.total_seconds()
+    value = seconds / divider
+    return value
+
+def finnishWeekdayOrder(weekday):
+    weekdayNumber = {'maanantai': 1, 'tiistai': 2, 'keskiviikko' : 3,
+     'torstai' : 4, 'perjantai': 5, 'lauantai': 6, 'sunnuntai': 7}
+    try:
+        value = f'{weekday} on viikon {finnishWeekdayOrder[weekday]}. päivä'
+    except Exception as error:
+        value = f'{weekday} ei ole viikonpäivä, tarkista syötteesi'
+    return value
+
+
+
 
 if __name__ == '__main__': # Kehityksen aikaisia kokeiluja / toimivuuden varmistamisia. (Että funktiot toimii) Nämä testit voi poistaa
 
@@ -89,6 +145,8 @@ if __name__ == '__main__': # Kehityksen aikaisia kokeiluja / toimivuuden varmist
     time2 = '15:25:00'
     ero = timediff2(time1, time2, 'minute')
     print('ero oli', ero, 'minuuttia')
+
+    print(dateTimeDiff('2023-04-28 10:00:00', '2023-04-29 11:00:00'), 'on v1')
    
 
    # Esimerkki  docstring käytöstä kun laitat vain datediff() menet () kohdalle se kertoo mitä tehdä
