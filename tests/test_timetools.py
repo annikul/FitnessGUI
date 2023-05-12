96657533dd264d8f576e9184c7ad4f6d612b9355
AnniK <122267866+annikul@users.noreply.github.com>
2023-05-12 18:04:39 +0300

    testiä

--- tests/test_timetools.py
+++ tests/test_timetools.py
@@ -9,3 +9,10 @@ import timetools
 def test_datediff():
     assert timetools.datediff('2023-04-28', '2023-04-10') == 18
     assert timetools.datediff('2023-04-10', '2023-04-28') == 18
+
+# Tälläisissa testeissä kannattaa laittaa joku round. Muuten testi ei välttämättä mene läpi. Pythonisssa on enempi desimaaleja kuin excelis
+# Test if timediff function calculates correct and absolute values. 
+def test_timediff():
+    assert round(timetools.timediff('11:30:15', '10:10:05'), 4) == 1.3361
+    assert round(timetools.timediff('10:10:05', '11:30:15'), 4) == 1.3361
+
